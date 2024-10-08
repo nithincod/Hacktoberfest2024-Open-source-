6c795c4b3f452483231bc2605bc7a0515f0ef4d9
nithincod <145334097+nithincod@users.noreply.github.com>
2024-10-09 03:37:55 +0530

    Create Matrix-chain-multiplication.cpp

---
+++ Matrix-chain-multiplication.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Matrix Ai has dimension arr[i-1] x arr[i]
+int minMultRec(vector<int>& arr, int i, int j) {
+  
+    // If there is only one matrix 
+    if (i + 1 == j)
+        return 0;
+
+    int res = INT_MAX;    
+      
+    // Place the first bracket at different
+    // positions or k and for every placed 
+    // first bracket, recursively compute 
+    // minimum cost for remaining brackets
+    // (or subproblems)
+    for (int k = i + 1; k < j; k++) {
+        int curr = minMultRec(arr, i, k)
+                  + minMultRec(arr, k, j)
+                  + arr[i] * arr[k] * arr[j];
+
+        res = min(curr, res);
+    }
+
+    // Return minimum count
+    return res;
+}
+
+int minMult(vector<int>& arr) {
+    int n = arr.size();
+    return minMultRec(arr, 0, n - 1);
+}
+
+int main() {
+    vector<int> arr = {1, 2, 3, 4, 3};
+    cout << minMult(arr);
+    return 0;
+}
